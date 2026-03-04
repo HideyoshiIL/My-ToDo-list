9dc0f298093c81488ea625b497a55b87a50130a0
HideyoshiIL <ilyha2000vip@yandex.ru>
2026-03-04 16:53:00 +0600

    Сделал ввод информации в список, добавил анимации  при наведении

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 "use strick"
 
+// Необходимо подвязать импут к кнопке после чего перенести текст в лист
+
+const inp = document.querySelector('#entry');
+const btnAdd = document.querySelector('.add');
+const ul = document.querySelector('.list');
+
+
+btnAdd.addEventListener('click', () => {
+  const text = inp.value;
+  const li = document.createElement('li');
+  li.textContent = text;
+
+  if (ul.children.length <= 25 && text.length <= 80) {
+  ul.append(li);
+  } else {
+    alert("Достигнут лимит задач или задача слишком длинная");
+  }
+
+  inp.value = "";
+});
