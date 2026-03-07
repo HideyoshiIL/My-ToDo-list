68433de2a375d450b6d6d3e99384ad5c6711ed16
HideyoshiIL <ilyha2000vip@yandex.ru>
2026-03-07 18:23:35 +0600

    Решил переделать форму

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 "use strick"
 
-// Необходимо подвязать импут к кнопке после чего перенести текст в лист
+const tasksDB = {
+  tasks: []
+};
 
 const inp = document.querySelector('#entry');
 const btnAdd = document.querySelector('.add');
 const ul = document.querySelector('.list');
+const checkBox = document.querySelector('[type="checkbox"]');
 
-
-btnAdd.addEventListener('click', () => {
+btnAdd.addEventListener('submit', (event) => {
+  event.preventDefault();
   const text = inp.value;
-  const li = document.createElement('li');
-  li.textContent = text;
 
-  if (ul.children.length <= 25 && text.length <= 80) {
-  ul.prepend(li);
-  } else {
-    alert("Достигнут лимит задач или задача слишком длинная");
-  }
+  // const li = document.createElement('li');
+  // li.textContent = text;
 
-  inp.value = "";
+  // if (ul.children.length <= 25 && text.length <= 80) {
+  // ul.prepend(li);
+  // } else {
+  //   alert("Достигнут лимит задач или задача слишком длинная");
+  // }
+
+  event.target.reset();
 });
